42a8e9ec25604ca99fa668c5e703032492ba8a14
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-11-12 20:46:08 +0500

    V3 изменен footer.blade.php

--- resources/views/pages/mainSections/footer.blade.php
+++ resources/views/pages/mainSections/footer.blade.php
@@ -1,35 +1,37 @@
 <p class="marging-title"></p>
-<footer class="container-fluid bg-primary d-flex justify-content-around flex-wrap col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 col-xxl-10">
-        <div class="col">
-            <h2 class="text-white fs-1" style="padding-bottom: 40px; ">ВОПЛОТИМ ВАШИ ИДЕИ ВМЕСТЕ</h2>
-            <p><b>Gallery 11, Студенческая ул., 11, Кировский район,
+<footer class="fool-window-width bg-light d-flex justify-content-around flex-wrap">
+    <div class="compressed-window-width marging-title d-flex justify-content-center flex-wrap" style="margin-bottom: 10vh">
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xl-6">
+            <h2 class="text-white fs-1 text-body" style="padding-bottom: 40px; ">ВОПЛОТИМ ВАШИ ИДЕИ ВМЕСТЕ</h2>
+            <p class="text-body"><b>Gallery 11, Студенческая ул., 11, Кировский район,
                     микрорайон Втузгородок, Екатеринбург, Россия</b>
             </p>
             <div class="d-flex row align-content-around" style="min-height: 190px">
                 <div >
                     <img src="{{asset('images/icons/tel.png')}}" width="30">
                     <span style="font-size: x-large; margin-left: 10px;">
-                        <a href=":tel" class="transition" style="text-decoration: none">8 912 688-08-92</a>
+                        <a href=":tel"  style="text-decoration: none">8 912 688-08-92</a>
                     </span>
                 </div>
                 <div>
                     <img src="{{asset('images/icons/insta.png')}}" width="30">
                     <span style="font-size: x-large; margin-left: 10px;">
-                        <a href="https://www.instagram.com/vozhev_design/" class="transition" style="text-decoration: none">@vozhev_design</a>
+                        <a href="https://www.instagram.com/vozhev_design/"  style="text-decoration: none">@vozhev_design</a>
                     </span>
                 </div>
                 <div>
                     <img src="{{asset('images/icons/vk.png')}}" width="30">
                     <span style="font-size: x-large; margin-left: 10px;">
-                        <a href="https://vk.com/design_vozhev" class="transition" style="text-decoration: none">DESIGN_VOZHEV</a>
+                        <a href="https://vk.com/design_vozhev"  style="text-decoration: none">DESIGN_VOZHEV</a>
                     </span>
                 </div>
-                <div class="btn btn-outline-primary fs-6 col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6 col-xxl-4" >
+                <div class="btn btn-outline-primary fs-6 col-12 col-sm-12 col-md-10 col-lg-6 col-xl-6 col-xxl-6" >
                     Заказать обратный звонок
                 </div>
             </div>
         </div>
-        <div class="d-flex justify-content-end d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">
-            <img  src="{{asset('images/content/footer.png')}}" width="500"/>
+        <div class="d-flex justify-content-end col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xl-6">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Adc8584d9c7183d13b5bbb1825ef3503f82cb60fa6c1d23a51a165af5e9c31978&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
         </div>
+    </div>
 </footer>
