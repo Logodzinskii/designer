5406eb61ec0dec5b187efde46fc1bd42e1859120
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-11-14 23:13:46 +0500

    V3 Изменены основные услуги

--- resources/views/pages/mainSections/services.blade.php
+++ resources/views/pages/mainSections/services.blade.php
@@ -1,41 +1,72 @@
-<section class="container-fluid bg-primary col-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-xxl-10">
-    <p class="fs-2 text-white marging-title">
+<section class="bg-primary fool-window-width d-flex justify-content-center align-items-center row flex-wrap marging-title" style="margin-bottom: 100px">
+    <div class="compressed-window-width d-flex align-self-center fs-1">
+        <div class="marging-title-border-bottom">
         ОСНОВНЫЕ УСЛУГИ
-    </p>
-    <div>
-        <div class="bg-primary accordion-item col-12 ">
-            <p class="fs-3 text-dark">Замер помещения</p>
-            <p class="fs-6">Имеется спорная точка зрения, гласящая примерно следующее: элементы политического процесса, вне зависимости от их уровня, должны быть своевременно верифицированы.</p>
-            <p class=" col-3 " id="panelsStayOpen-headingOne">
-                <button class="bg-primary text-white accordion-button-light fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
-                    подробнее
-                </button>
-            </p>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body text-dark">
-                    <ul class="list-unstyled">
-                        <li>— Встреча с дизайнером на объекте</li>
-                        <li>— Фото и видеофиксация помещений</li>
-                        <li>— Составление технического задания</li>
-                    </ul>
-                </div>
+        </div>
+    </div>
+    <div class="compressed-window-width d-flex justify-content-between d-flex justify-content-center  flex-wrap">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-start align-items-start" >
+            <div class="bg-warning square-content d-flex row" >
+                <p class="fs-3 text-body">ДИЗАЙН <br/> ПРОЕКТ</p>
+                <ul class="d-flex row align-items-baseline list-unstyled">
+                    <li>— Замер помещения</li>
+                    <li>— Разработка планировочных решений</li>
+                    <li>— 3D визуализация помещений</li>
+                    <li>— Техническая документация</li>
+                    <li>— Комплектация проекта</li>
+                    <li>— Смета строительных работ</li>
+                    <li>— Состав материалов с добавлением, мебель и оборудование, и где закупка!</li>
+                </ul>
+                <button class="btn btn-info align-self-end">СКАЧАТЬ ПРИМЕР</button>
             </div>
         </div>
-        <div class="bg-primary accordion-item col-12">
-            <p class="fs-3 text-dark"> Разработка планировочных решений</p>
-            <p class="fs-6">Повседневная практика показывает, что высокое качество позиционных исследований способствует повышению качества экспериментов, поражающих по своей масштабности и грандиозности.</p>
-            <p class=" col-3 " id="panelsStayOpen-heading2">
-                <button class="bg-primary text-white accordion-button-light fs-6" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse2" aria-expanded="false" aria-controls="panelsStayOpen-collapse2">
-                    подробнее
-                </button>
-            </p>
-            <div id="panelsStayOpen-collapse2" class="accordion-collapse collapse " aria-labelledby="panelsStayOpen-heading2">
-                <div class="accordion-body text-dark">
-                    <ul class="list-unstyled">
-                        <li>— Встреча с дизайнером на объекте</li>
-                        <li>— Фото и видеофиксация помещений</li>
-                        <li>— Составление технического задания</li>
-                    </ul>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-end align-items-start" >
+            <div class="bg-info square-content d-flex row align-items-baseline" >
+                <p class="fs-3 text-white">БЕСПЛАТНАЯ <br/> КОНСУЛЬТАЦИЯ</p>
+                <ul class="d-flex row align-items-baseline list-unstyled">
+                    <li>— Разберем планировочное решение квартиры</li>
+                    <li>— Определим стиль будущего интерьера</li>
+                    <li>— Производим прибыль дизайн проекта</li>
+                    <li>— Рекомендации от дизайнера</li>
+                    <li>— Ответим на ваши вопросы</li>
+                </ul>
+                <button class="btn btn-info align-self-end">ЗАПИСАТЬСЯ</button>
+            </div>
+        </div>
+    </div>
+    <div class="compressed-window-width d-flex justify-content-between d-flex justify-content-center flex-wrap">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-start align-items-end" >
+            <div class="bg-info square-content row align-items-baseline" >
+                <p class="fs-3 text-white">АВТОРСКИЙ <br/> НАДЗОР</p>
+                <ul class="d-flex row align-items-baseline list-unstyled">
+                    <li>— Еженедельное посещение объекта</li>
+                    <li>— Надзор за начальными строительно-отделочными работами</li>
+                    <li>— Проверка проектной документации подрядных организаций</li>
+                    <li>— Контроль исполнительской деятельности подрядных организаций</li>
+                    <li>— Предоставление прибыли о выполненных работах на объекте</li>
+                    <li>— Оперативное решение спорных вопросов</li>
+                    <li>— Доукомплектация объекта</li>
+                    <li>— Профессиональная фотофиксация готового интерьера
+                        (с семьей)</li>
+                </ul>
+                <button class="btn btn-info d-flex align-self-center">30 000 Р/МЕС.</button>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-end align-items-end" >
+            <div class="bg-light square-content">
+                <div class="row d-flex align-items-center" style="height: 100%;">
+                    <div style="position: absolute; top: 0; width: 100%; height: 100%; opacity: 0.7;">
+
+                    </div>
+                    <div class="col d-flex justify-content-center align-items-center" style="opacity: 0.5">
+                        <img src="{{asset('images/icons/main_index_logo.png')}}" height="60">
+                    </div>
+                    <h1 class="fs-1 text-body align-self-center text-center" style="z-index: 2">
+                        <a href="">ЗАКАЗАТЬ ПРОЕКТ</a>
+                    </h1>
+                    <script src="https://yastatic.net/share2/share.js"></script>
+                    <div class="ya-share2 align-self-center d-flex justify-content-center" data-curtain data-size="m" data-shape="round" data-color-scheme="blackwhite" data-services="messenger,vkontakte,odnoklassniki,telegram,twitter,viber,whatsapp">
+                    </div>
                 </div>
             </div>
         </div>
