760fa7ab2bc41354813818f3c4e4bb8248cebf7f
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-11-20 14:13:08 +0500

    V3 Исправлена подвал

--- resources/views/pages/mainSections/footer.blade.php
+++ resources/views/pages/mainSections/footer.blade.php
@@ -1,34 +1,30 @@
 <p class="marging-title"></p>
 <footer class="fool-window-width bg-light d-flex justify-content-around flex-wrap">
     <div class="compressed-window-width marging-title d-flex justify-content-center flex-wrap">
-        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xl-6">
+        <div class="d-flex justify-content-start align-items-end row col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xl-6">
             <h2 class="text-white fs-1 text-body" style="padding-bottom: 40px; ">ВОПЛОТИМ ВАШИ ИДЕИ ВМЕСТЕ</h2>
-            <p class="text-body"><b>Gallery 11, Студенческая ул., 11, Кировский район,
+            <p class="text-body fs-5"><b>Gallery 11, Студенческая ул., 11, Кировский район,
                     микрорайон Втузгородок, Екатеринбург, Россия</b>
             </p>
-            <div class="d-flex row align-content-around" style="min-height: 190px">
-                <div >
-                    <img src="{{asset('images/icons/tel.png')}}" width="30">
-                    <span style="font-size: x-large; margin-left: 10px;">
-                        <a href=":tel"  style="text-decoration: none">8 912 688-08-92</a>
-                    </span>
-                </div>
-                <div>
-                    <img src="{{asset('images/icons/insta.png')}}" width="30">
-                    <span style="font-size: x-large; margin-left: 10px;">
-                        <a href="https://www.instagram.com/vozhev_design/"  style="text-decoration: none">@vozhev_design</a>
-                    </span>
-                </div>
-                <div>
-                    <img src="{{asset('images/icons/vk.png')}}" width="30">
-                    <span style="font-size: x-large; margin-left: 10px;">
-                        <a href="https://vk.com/design_vozhev"  style="text-decoration: none">DESIGN_VOZHEV</a>
-                    </span>
-                </div>
-                <div class="btn btn-outline-primary fs-6 col-12 col-sm-12 col-md-10 col-lg-6 col-xl-6 col-xxl-6" >
-                    Заказать обратный звонок
-                </div>
+            <p>
+                <b>
+                    <a class="link-dark fs-5" href=":tel"  style="text-decoration: none">8 912 688-08-92</a>
+                </b>
+            </p>
+            <div class="d-flex justify-content-start align-items-center">
+                <a href="https://wa.me/89126880892" class="link-dark">
+                    <img src="images/icons/social/whatsapp-svgrepo-com.svg" alt="" width="30" height="30" />
+                </a>
+                <a href="https://www.instagram.com/vozhev_design/" class="link-dark">
+                    <img src="images/icons/social/instagram-icon-svgrepo-com.svg" alt="" width="28" height="28" />
+                </a>
+                <a href="https://vk.com/design_vozhev" class="link-dark">
+                    <img src="images/icons/social/vk-alt-svgrepo-com.svg" alt="" width="30" height="30" />
+                </a>
             </div>
+            <button class="btn btn-outline-primary fs-6 col-12 col-sm-12 col-md-12 col-lg-10 col-xl-6 col-xxl-6" >
+                Заказать обратный звонок
+            </button>
         </div>
         <div class="d-flex justify-content-end col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xl-6">
             <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Adc8584d9c7183d13b5bbb1825ef3503f82cb60fa6c1d23a51a165af5e9c31978&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
