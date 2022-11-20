a89f53f2456d736ce72fa31337750c7261d51f3a
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-11-20 11:00:07 +0500

    V3 добавлен тест блока преимущества

--- resources/views/pages/mainSections/positive.blade.php
+++ resources/views/pages/mainSections/positive.blade.php
@@ -1,4 +1,4 @@
-<section class="fool-window-width row d-flex justify-content-center align-content-around" >
+<section class="fool-window-width row d-flex justify-content-center flex-wrap align-content-around" >
     @php
         $arr=[
             ['title'=>'СКОРОСТЬ РЕАЛИЗАЦИИ',
@@ -20,10 +20,10 @@
                 ];
     @endphp
 
-    <div class="compressed-window-width d-flex justify-content-center">
-            <div class="d-flex justify-content-between flex-wrap col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style="height: 60vh; margin-top: 10vh;">
+    <div class="compressed-window-width d-flex flex-wrap justify-content-center" style="min-height: 60vh;">
+            <div class="d-flex justify-content-between col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style="height: 60vh; margin-top: 10vh;">
                 <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 difference" >
-                    <div class="text-dark fs-4 d-flex align-content-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
+                    <div class="text-dark fs-4 d-flex align-items-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
                         <h2>{{$arr[0]['title']}}</h2>
                     </div>
                     <div class="text-white fs-6 align-middle difference-body" style="padding-left: 5px; padding-right: 5px">
@@ -31,7 +31,7 @@
                     </div>
                 </div>
                 <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 difference" >
-                    <div class="text-dark fs-4 d-flex align-content-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
+                    <div class="text-dark fs-4 d-flex align-items-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
                         <h2>{{$arr[1]['title']}}</h2>
                     </div>
                     <div class="text-white fs-6 align-middle difference-body" style="padding-left: 5px; padding-right: 5px">
@@ -39,9 +39,9 @@
                     </div>
                 </div>
             </div>
-            <div class="d-flex justify-content-between flex-wrap col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style="height: 60vh; margin-top: 10vh;">
+            <div class="d-flex justify-content-between col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style="height: 60vh; margin-top: 10vh;">
                 <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 difference" >
-                    <div class="text-dark fs-4 d-flex align-content-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
+                    <div class="text-dark fs-4 d-flex align-items-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
                         <h2>{{$arr[2]['title']}}</h2>
                     </div>
                     <div class="text-white fs-6 align-middle difference-body" style="padding-left: 5px; padding-right: 5px">
@@ -49,7 +49,7 @@
                     </div>
                 </div>
                 <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 difference" >
-                    <div class="text-dark fs-4 d-flex align-content-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
+                    <div class="text-dark fs-4 d-flex align-items-center difference-header text-center" style="padding-left: 5px; padding-right: 5px">
                         <h2>{{$arr[3]['title']}}</h2>
                     </div>
                     <div class="text-white fs-6 align-middle difference-body" style="padding-left: 5px; padding-right: 5px">
