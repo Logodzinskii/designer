f448d34f3f20087e302ea899acc930269d059f7a
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-11-21 23:05:52 +0500

    V3 Изменены основные услуги

--- resources/views/pages/mainSections/services.blade.php
+++ resources/views/pages/mainSections/services.blade.php
@@ -4,139 +4,124 @@
         ОСНОВНЫЕ УСЛУГИ
         </div>
     </div>
-    <div class="compressed-window-width d-flex justify-content-between d-flex justify-content-center  flex-wrap">
+    <div class="compressed-window-width d-flex justify-content-between justify-content-center flex-wrap" style="min-height: 100%">
         <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-start align-items-start" >
-            <div class="bg-warning square-content d-flex row" >
+            <div class="bg-warning square-content d-flex row justify-content-end" >
                 <p class="fs-3 text-body">ДИЗАЙН <br/> ПРОЕКТ</p>
-                <ul class="d-flex row align-items-baseline list-unstyled">
+                <ul class="d-flex row align-items-baseline list-unstyled p-0 m-0">
                     <li class="d-flex justify-content-start">
-                        — Замер помещения
-                        <span class="bg-info rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                        <span style="cursor:pointer;"
                               data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                         — Встреча с дизайнером на объекте<br>
                         — Фото и видеофиксация помещений<br>
                         — Составление технического задания">
-                            i
+                            <u>Замер помещения</u>
                         </span>
                     </li>
                     <li class="d-flex justify-content-start">
-                        — Разработка планировочных решений
-                        <span class="bg-info rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                        <span style="cursor:pointer;"
                               data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                         — Обмерный план - отражает корректные замеры Вашей квартиры с указанием расположения всех коммуникаций<br>
                         — План перепланировки стен - (если предусмотрено проектом)<br>
                         — План расстановки мебели с размерами">
-                            i
+                            <u>Разработка планировочных решений</u>
                         </span>
                     </li>
                     <li class="d-flex justify-content-start">
-                        — 3D визуализация помещений
-                        <span class="bg-info rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                        <span style="cursor:pointer;"
                               data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                             —  Реалистичная визуализация будущего интерьера<br>
                             —  Сочетание форм, цветов, текстур, геометрии">
-                            i
+                            <u>3D визуализация помещений</u>
                         </span>
                     </li>
                     <li class="d-flex justify-content-start">
-                        — Техническая документация
-                        <span class="bg-info rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                        <span style="cursor:pointer;"
                               data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                         — Согласование спецификации и последующая корректировка без отхождения от утвержденной 3D визуализации<br>
                         — График поставки от поставщиков по каждой согласованной позиции в GooglDisk<br>
                         — Подготовка и сбор всех финишных отделочных материалов у подрядчиков или у нас в офисе<br>">
-                            i
+                            <u>Техническая документация</u>
                         </span>
                     </li>
-                    <li>— Комплектация проекта</li>
-                    <li>— Смета строительных работ</li>
-                    <li>— Состав материалов с добавлением, мебель и оборудование, и где закупка!</li>
+                    <li>Комплектация проекта</li>
+                    <li>Смета строительных работ</li>
+                    <li>Состав материалов с добавлением, мебель и оборудование, и где закупка!</li>
                 </ul>
-                <button class="btn btn-info align-self-end">СКАЧАТЬ ПРИМЕР</button>
+                <button class="btn btn-secondary align-self-end col-8">СКАЧАТЬ ПРИМЕР</button>
             </div>
         </div>
-        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-end align-items-start" >
-            <div class="bg-info square-content d-flex row align-items-baseline" >
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-start align-items-start" >
+            <div class="bg-info square-content d-flex row justify-content-end" >
                 <p class="fs-3 text-white">БЕСПЛАТНАЯ <br/> КОНСУЛЬТАЦИЯ</p>
-                <ul class="d-flex row align-items-baseline list-unstyled">
-                    <li>— Разберем планировочное решение квартиры</li>
-                    <li>— Определим стиль будущего интерьера</li>
-                    <li>— Производим прибыль дизайн проекта</li>
-                    <li>— Рекомендации от дизайнера</li>
-                    <li>— Ответим на ваши вопросы</li>
+                <ul class="d-flex row align-items-baseline list-unstyled p-0 m-0">
+                    <li>Разберем планировочное решение квартиры</li>
+                    <li>Определим стиль будущего интерьера</li>
+                    <li>Производим прибыль дизайн проекта</li>
+                    <li>Рекомендации от дизайнера</li>
+                    <li>Ответим на ваши вопросы</li>
                 </ul>
-                <button class="btn btn-info align-self-end">ЗАПИСАТЬСЯ</button>
+                <button class="btn btn-info align-self-end col-8">ЗАПИСАТЬСЯ</button>
             </div>
         </div>
     </div>
     <div class="compressed-window-width d-flex justify-content-between d-flex justify-content-center flex-wrap">
-        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-start align-items-end" >
-            <div class="bg-info square-content row align-items-baseline" >
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-start align-items-start" >
+            <div class="bg-info square-content d-flex row justify-content-end" >
                 <p class="fs-3 text-white">АВТОРСКИЙ <br/> НАДЗОР</p>
-                <ul class="d-flex row align-items-baseline list-unstyled">
+                <ul class="d-flex row align-items-baseline list-unstyled p-0 m-0">
                     <li class="d-flex justify-content-start">
-                        — Еженедельное посещение объекта
-                    <span class="bg-warning rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                    <span style="cursor:pointer;"
                           data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                         — Выезды на объект для уточнения размеров после сноса старых и возведения новых перегородок, после выравнивания стен, установки дверных полотен и коробов<br>
                         — Выезды на объект для корректировки проекта и внесения изменений в техническую документацию<br>
                         — Оперативное решение вопросов по изменениям и дополнениям в проекте">
-                            i
+                        <u>Еженедельное посещение объекта</u>
                         </span>
                     </li>
                     <li class="d-flex justify-content-start">
-                        — Надзор за начальными строительно-отделочными работами
-                        <span class="bg-warning rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                        <span style="cursor:pointer;"
                               data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                         — Надзор за соответствием строительно-отделочных работ по проекту или отдельным его частям, таких как: укладка паркета, стыковка узора керамогранита, поклейка обоев, покраска и декорирование стен (фактур, текстур)<br>
                         — Надзор за соответствием цветовой гаммы утвержденному цветовому решению<br>
                         — Надзор за расстановкой мебели, оборудования и декоративных элементов<br>
                         — Необходимое общение с прорабом на объекте и по телефону, консультации по всем предоставленным чертежам<br>">
-                            i
+                            <u>Надзор за начальными строительно-отделочными работами</u>
                         </span>
                     </li>
-                    <li>— Проверка проектной документации подрядных организаций</li>
-                    <li>— Контроль исполнительской деятельности подрядных организаций</li>
+                    <li>Проверка проектной документации подрядных организаций</li>
+                    <li>Контроль исполнительской деятельности подрядных организаций</li>
                     <li class="d-flex justify-content-start">
-                        — Предоставление прибыли о выполненных работах на объекте
-                        <span class="bg-warning rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                        <span style="cursor:pointer;"
                               data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                         —  Переговоры с подрядными организациями-поставляющие изделия или отдельные материалы на объект: мебель, световое оборудование, отделочные материалы<br>
                         —  Надзор за расстановкой мебели, оборудованием и декоративных элементов<br>">
-                            i
+                            <u>Предоставление прибыли о выполненных работах на объекте</u>
                         </span>
                     </li>
-                    <li>— Оперативное решение спорных вопросов</li>
+                    <li>Оперативное решение спорных вопросов</li>
                     <li class="d-flex justify-content-start">
-                        — Доукомплектация объекта
-                        <span class="bg-warning rounded-circle d-flex justify-content-center align-items-center" style="cursor:pointer;margin-left:10px; width: 20px;height: 20px; display: block;"
+                        <span style="cursor:pointer;"
                               data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="
                         — Индивидуальное сопровождение в подборе и закупе чистовых материалов и мебели">
-                            i
+                            <u>Доукомплектация объекта</u>
                         </span>
                     </li>
-                    <li>— Профессиональная фотофиксация готового интерьера
+                    <li>Профессиональная фотофиксация готового интерьера
                         (с семьей)</li>
                 </ul>
-                <button class="btn btn-info d-flex align-self-center">30 000 Р/МЕС.</button>
+                <button class="btn btn-info align-self-end col-8">25 000 Р/МЕС.</button>
             </div>
         </div>
-        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-end align-items-end" >
-            <div class="bg-warning square-content">
-                <div class="row d-flex align-items-center" style="height: 100%;">
-                    <div style="position: absolute; top: 0; width: 100%; height: 100%; opacity: 0.7;">
-
-                    </div>
-                    <div class="col d-flex justify-content-center align-items-center" style="opacity: 0.5">
-                        <img src="{{asset('images/icons/main_index_logo.png')}}" height="60">
-                    </div>
-                    <h1 class="fs-1 text-body align-self-center text-center" style="z-index: 2">
-                        <a href="" class="text-body">ЗАКАЗАТЬ ПРОЕКТ</a>
-                    </h1>
-                    <script src="https://yastatic.net/share2/share.js"></script>
-                    <div class="ya-share2 align-self-center d-flex justify-content-center" data-curtain data-size="m" data-shape="round" data-color-scheme="blackwhite" data-services="messenger,vkontakte,odnoklassniki,telegram,twitter,viber,whatsapp">
-                    </div>
-                </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 square d-flex justify-content-start align-items-start" >
+            <div class="bg-warning square-content d-flex row align-items-center justify-content-end" >
+                <p class="fs-3 text-body">ХОТИТЕ НАЧАТЬ ПРОЕКТ?</p>
+                <ul class="d-flex row align-items-baseline list-unstyled p-0 m-0">
+                    <li class="d-flex justify-content-start">Оставьте заявку,</li>
+                    <li>вам перезвонит руководитель студии,</li>
+                    <li>чтобы ответить на все вопросы</li>
+                </ul>
+                <button class="btn btn-secondary align-self-end col-8">СКАЧАТЬ ПРИМЕР</button>
             </div>
         </div>
     </div>
